7ef85775b7c6c6f9d96b9f23297006914ad0b26a
Felix2311 <f.ramirezl@fia.unicit.edu.ni>
2025-11-22 22:46:27 -0600

    feature: Instalacion de dependencias para formatear codigo con eslint y prettier

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+  Pressable,
+} from "react-native";
+import icon from "./assets/splash-icon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={icon}
+        style={{
+          width: 100,
+          height: 100,
+          resizeMode: "center",
+        }}
+      />
+      <Text style={{ color: "white" }}>Paydrive esta funcionando!</Text>
+      <Button
+        color="#4e5"
+        title="Boton sospechoso"
+        onPress={() => alert("Probando alertas")}
+      ></Button>
+
+      <TouchableHighlight underlayColor={"#4e5"}>
+        <Text>Vamos</Text>
+      </TouchableHighlight>
+      <Pressable>
+        {({ pressed }) => (
+          <Text
+            style={{
+              fontSize: pressed ? 45 : 16,
+            }}
+          >
+            {pressed ? "Boton precionado" : "Boton sin Presionar"}
+          </Text>
+        )}
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgba(68, 152, 221, 1)",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
