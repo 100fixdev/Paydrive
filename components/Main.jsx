8414ceeaac10fef8721579647c11b07a37f026b5
Felix2311 <f.ramirezl@fia.unicit.edu.ni>
2025-12-03 22:41:01 -0600

    feat: cracion de componente para el listado de simulaciones

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,53 +7,86 @@ import {
   Button,
   TouchableHighlight,
   Pressable,
-  ScrollView
+  FlatList,
 } from "react-native";
 import icon from "../assets/splash-icon.png";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-
 export function Main() {
-const insets = useSafeAreaInsets();
+  const insets = useSafeAreaInsets();
 
   return (
-    <View style={[{paddingTop: insets.top}, {paddingBottom: insets.bottom}]}>
-    <ScrollView>
-      <Image
-        source={icon}
-        style={styles.Image}
-      />
-      <Text style={{ color: "white" }}>Paydrive esta funcionando!</Text>
-      <Button
-        color="#4e5"
-        title="Boton sospechoso"
-        onPress={() => alert("Probando alertas")}
-      ></Button>
+    
+    <View style={styles.body}>
+      <View style={styles.container__card}>
+      <Text style={styles.title}>Mis Simulaciones</Text>
 
-      <TouchableHighlight underlayColor={"#4e5"}>
-        <Text>Vamos</Text>
-      </TouchableHighlight>
-      <Pressable>
-        {({ pressed }) => (
-          <Text
-            style={{
-              fontSize: pressed ? 45 : 16,
-            }}
-          >
-            {pressed ? "Boton precionado" : "Boton sin Presionar"}
-          </Text>
-        )}
-      </Pressable>
+        <View style={styles.card}>
+          <Pressable>
+            <Text>Simulacion X</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.container__agregar}>
+        <Pressable style={styles.btn__agregar}>
+          <Text style={styles.txt__agregar}>+</Text>
+        </Pressable>
+        <Text>Agregar Simulacion</Text>
+      </View>
       <StatusBar style="auto" />
-    </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({  
+  body: {
+    flex: 1,
+    backgroundColor: "rgba(206, 157, 51, 1)",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  container__card:{
+    width: "100%",
+    alignItems: "center",
+  },
   Image: {
     width: 100,
     height: 100,
     resizeMode: "center",
+  },
+  card:{
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    borderWidth: 2,
+    borderColor: "black",
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+  container__agregar:{
+    alignItems: "center",
+    marginTop: 20,
+    backgroundColor: "green",
+  },
+  btn__agregar:{
+    backgroundColor: "crimson",
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  txt__agregar:{
+    color: "white",
+    fontSize: 40,
   },
 });
