6e1311e17658d71807abb44ab6313f4bdc6c21c5
Felix2311 <f.ramirezl@fia.unicit.edu.ni>
2025-12-07 14:54:38 -0600

    feat: iconos de react en tarjetas

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,9 +1,11 @@
 import { Text } from "react-native";
 import CardBase from "./CardBase";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function ProductCard({ title, onPress }) {
     return (
         <CardBase onPress={onPress}>
+            <Icon name="cube" size={24} color="black" />
             <Text style={{ fontSize: 16, fontWeight: "bold" }}>{title}</Text>
         </CardBase>
     );
